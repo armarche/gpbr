d2598d2ba0a4c830428d2703595fb4d30d8e115a
arkma <marchenko.arkadii@gmail.com>
2024-10-07 20:45:16 +0300

    AP-13 cleanup

--- gpbr/gpbr/mfs/polinomial.py
+++ gpbr/gpbr/mfs/polinomial.py
@@ -88,16 +88,6 @@ def calculate_2d_polinomials(mfs_data: MfSData, N: int) -> MFSPolinomials2D:
     v_polynomials = [Polynomial([1])] # v_0(r)=1
     w_polynomials = [Polynomial([0])] # w_0(r)=0
 
-    # for n in range(1,N+1):
-    #     v_n_coeff = np.zeros(n+1)
-    #     for m in range(0, np.trunc(n/2)):
-    #         v_n_coeff[2*m] = A[n, 2*m]
-    #     v_polynomials.append(Polynomial(v_n_coeff))
-
-    #     w_n_coeff = np.zeros(n+1)
-    #     for m in range(0, np.trunc((n-1)/2)):
-    #         v_n_coeff[2*m+1] = A[n, 2*m+1]
-    #     w_polynomials.append(Polynomial(w_n_coeff))
     for n in range(1,N+1):
         v_n_coeff = np.zeros(n+1)
         for m in range(0, int(n/2)+1):
@@ -111,62 +101,3 @@ def calculate_2d_polinomials(mfs_data: MfSData, N: int) -> MFSPolinomials2D:
 
     return MFSPolinomials2D(A, v_polynomials, w_polynomials)
 
-
-""" # Delete laters
-def calculate_3d_polinomials(mfs_data: MfSData, N: int) -> MFSPolinomials3D:
-    def polinomial_coeff(n: int,k:int, A: np.ndarray) -> np.float64:
-        res = k*(k+1)*A[n,k+1]
-        for m in range(k-1, n): # m = k-1;n-1
-            res -=mfs_data.Beta[n-m]*A[m, k-1] # TODO: test beta
-        res /=(2*mfs_data.nu*k)
-        return res
-
-    # a_nm, n=0,1,...N, m =0,1,...n
-    A = np.empty((N+1,N+1))
-
-    # Fill nan to be sure we don't use indexes we don't need
-    A[:] = np.nan
-
-    ## Fill first column
-    A[:,0]=1
-
-    ## Fill diagonal elements
-    for n in range(1,N+1):
-        A[n,n] = -(mfs_data.Beta[1]*A[n-1,n-1])/(2*mfs_data.nu*n)
-
-    polinomials = []
-    for n in range(N+1):
-        for m in range(n-1, 0, -1):
-            A[n,m] = polinomial_coeff(n,m,A)
-        polinomials.append(Polynomial(A[n,:n+1]))
-    return MFSPolinomials3D(A, np.array(polinomials, dtype=Polynomial))
-
-
-def calculate_2d_polinomials(mfs_data: MfSData, N: int) -> MFSPolinomials3D:
-    def polinomial_coeff(n: int,k:int, A: np.ndarray) -> np.float64:
-        res = (4*np.trunc((k+1)/2)**2)*A[n,k+1]
-        for m in range(k-1, n): # m = k-1;n-1
-            res -=mfs_data.Beta[n-m]*A[m, k-1] # TODO: test beta
-        res /=(2*mfs_data.nu*k)
-        return res
-
-    # a_nm, n=0,1,...N, m =0,1,...n
-    A = np.empty((N+1,N+1))
-
-    # Fill nan to be sure we don't use indexes we don't need
-    A[:] = np.nan
-
-    ## Fill first column
-    A[:,0]=1
-
-    ## Fill diagonal elements
-    for n in range(1,N+1):
-        A[n,n] = -(mfs_data.Beta[1]*A[n-1,n-1])/(2*mfs_data.nu*n)
-
-    polinomials = []
-    for n in range(N+1):
-        for m in range(n-1, 0, -1):
-            A[n,m] = polinomial_coeff(n,m,A)
-        polinomials.append(Polynomial(A[n,:n+1]))
-    return MFSPolinomials3D(A, np.array(polinomials, dtype=Polynomial))
-"""
